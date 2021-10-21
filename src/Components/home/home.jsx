8c7fa07bb6d63ec9449feba46d261de7addf8c70
namitkashi <91289904+namitkashi@users.noreply.github.com>
2021-10-22 00:26:04 +0530

    Update home.jsx

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -39,34 +39,21 @@ export function Home() {
 			<div className='row'>
 				<Box
 					title='Sorting Algorithms (Arrays)'
-					link='https://jindal2209.github.io/Sorting_Visualizer/'
+					link='https://namitkashi.github.io/Sorting_Visualizer/'
 					text='Visualization of various sorting algorithms like QuickSort, MergeSort, BubbleSort..'
 				/>
-				<Box
-					title='Flood Fill Algorithm (Graphs)'
-					link='https://jindal2209.github.io/FloodFill_Visualizer/'
-					text='Visualization of Flood Fill Algorithm used in paint buckets using Depth First Search(DFS) and Breadth First Search(BFS)'
-				/>
+				
 				<Box
 					title='N-Queen Problem (Backtracking)'
-					link='https://jindal2209.github.io/nqueen/'
+					link='https://namitkashi.github.io/nqueen/'
 					text='Visualization of N-Queen problem using backtracking'
 				/>
 				<Box
 					title='Text Editor With Auto Suggestion (Tries, Graphs, HashMaps)'
-					link='https://jindal2209.github.io/Text_Editor_Auto_Suggestion/'
+					link='https://namitkashi.github.io/Text_Editor_Auto_Suggestion/'
 					text='A text editor that also provides suggestion based on previous text entered by the user. Data Structures used Tries, Maps, Graphs.'
 				/>
-				<Box
-					title='Transaction Settlement (Heaps)'
-					link='https://jindal2209.github.io/transaction_settlement_splitwise/'
-					text='An application that allows consumers to split expenses with friends similar to split-wise. Implemented using heaps. Implemented heap data structure in java-script.'
-				/>
-				<Box
-					title='..................................'
-					link='https://in.linkedin.com/in/Shubham-jindal-116431193'
-					text='More will be added as soon as I get some more ideas. Meanwhile you can view my profile. Will be happy to collaborate :-)'
-				/>
+				
 			</div>
 		</div>
 	)
